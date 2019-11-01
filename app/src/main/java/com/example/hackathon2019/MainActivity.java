54b64df07d11f5573ace0651cfd3285816a3fb20
jiwon_h98@naver.com <jiwon_h98@naver.com>
2019-11-02 05:34:01 +0900

    수정

--- app/src/main/java/com/example/hackathon2019/MainActivity.java
+++ app/src/main/java/com/example/hackathon2019/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
@@ -26,14 +27,25 @@ public class MainActivity extends AppCompatActivity {
     private Socket socket;
     BufferedReader in;
     PrintWriter out;
-    EditText input;
+    Button share;
     Button button;
     TextView output;
     String data;
 
     String name;
 
-    double[] location;
+
+    double curLat;
+    double curLng;
+
+    String[] loc;
+    String lat;
+    String lng;
+    Double dlat;
+    Double dlng;
+    Double totallat = 0.0;
+    Double totallng = 0.0;
+    int count = 0;
 
 
     @Override
@@ -44,69 +56,57 @@ public class MainActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         name = bundle.getString("name");
 
-        input = (EditText) findViewById(R.id.input);
+        share = (Button) findViewById(R.id.shareButton);
         button = (Button) findViewById(R.id.button);
         output = (TextView) findViewById(R.id.output);
 
         StrictMode.enableDefaults();
 
-        input.setOnTouchListener(new View.OnTouchListener() {
+
+        curLat = 37.5443003;
+        curLng = 126.9722328;
+
+        share.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                input.getText().clear();
-                return false;
+            public void onClick(View v) {
+
             }
         });
 
         Thread worker = new Thread() {
             public void run() {
                 try {
-                    socket = new Socket("172.20.10.2", 9000);
+                    socket = new Socket("172.20.10.2", 5555);
                     out = new PrintWriter(socket.getOutputStream(), true);
                     in = new BufferedReader(new InputStreamReader(
                             socket.getInputStream()));
-
-                    double lat = 37.5443003;
-                    double lng = 126.9722328;
-                    out.print(name + "," + String.valueOf(lat) + "," + String.valueOf(lng) + "\n");
+                    out.print(name+"\n");
                     out.flush();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
 
-                /*try {
-                    while (true) {
-                        data = in.readLine();
-                        output.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                output.append("\n" + data);
-                            }
-                        });
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }*/
 
                 while (true) {
                     try {
                         data = in.readLine();
-                        if(data.contains("location >> ")) {
-                            data = data.substring(11);
-                            String[] test;
-                            test = data.split(", ");
-                            location[0] = Double.parseDouble(test[0]);
-                            location[1] = Double.parseDouble(test[1]);
-                            Toast.makeText(getApplicationContext(), test[0],Toast.LENGTH_LONG).show();
-                            Toast.makeText(getApplicationContext(), test[1],Toast.LENGTH_LONG).show();
-                        } else {
+                        loc = data.split(">>");
+                        lat = loc[1].split(",")[0];
+                        lng = loc[1].split(",")[1];
+                        dlat = Double.parseDouble(lat);
+                        dlng = Double.parseDouble(lng);
+                        totallat += dlat;
+                        totallng += dlng;
+                        count++;
+
                             output.post(new Runnable() {
                                 @Override
                                 public void run() {
                                     output.append("\n" + data);
+
                                 }
                             });
-                        }
 
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -117,12 +117,12 @@ public class MainActivity extends AppCompatActivity {
 
         worker.start();
 
-        button.setOnClickListener(new View.OnClickListener() {
+        share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new Thread() {
                     public void run() {
-                        String data = input.getText().toString();
+                        String data = String.valueOf(curLat) + "," + String.valueOf(curLng);
                         Log.w("NETWORK", "" + data);
                         if (data != null) {
                             out.print(data + "\n");
@@ -131,6 +131,21 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 }.start();
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putDouble("latitude", totallat/count);
+                bundle.putDouble("longitude", totallng/count);
+                Intent intent = new Intent(MainActivity.this, MapPoi.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+                finish();
+                //Toast.makeText(getApplicationContext(), String.valueOf(totallat), Toast.LENGTH_LONG).show();
+                //Toast.makeText(getApplicationContext(), String.valueOf(totallng), Toast.LENGTH_LONG).show();
             }
         });
     }
--- app/src/main/java/com/example/hackathon2019/MapPoi.java
+++ app/src/main/java/com/example/hackathon2019/MapPoi.java
@@ -12,7 +12,11 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 import com.skt.Tmap.TMapData;
 import com.skt.Tmap.TMapGpsManager;
@@ -29,67 +33,103 @@ public class MapPoi extends AppCompatActivity implements TMapGpsManager.onLocati
         TMapView tmapView;
         double latitude;
         double longitude;
-private TMapGpsManager tmapgps = null;
+        private TMapGpsManager tmapgps = null;
 
-@Override
-public void onLocationChange(Location location) {
-        latitude = location.getLatitude();
-        longitude = location.getLongitude();
-        tmapView.setLocationPoint(longitude, latitude);
-        tmapView.setCenterPoint(longitude, latitude);
+        @Override
+        public void onLocationChange(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                tmapView.setLocationPoint(longitude, latitude);
+                tmapView.setCenterPoint(longitude, latitude);
         }
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.map);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.map);
 
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                final double avglat;
+                final double avglng;
 
-        linearLayoutTmap = (LinearLayout) findViewById(R.id.linearLayoutTmap);
-        tmapView = new TMapView(this);
+                Bundle bundle = getIntent().getExtras();
+                avglat = bundle.getDouble("latitude");
+                avglng = bundle.getDouble("longitude");
 
-        tmapView.setSKTMapApiKey("42856b74-3604-474f-ad72-df6d1b5aaca0");
-        linearLayoutTmap.addView(tmapView);
+                final String[] list = {"숙박", "쇼핑", "주요시설물", "은행", "ATM", "편의점",
+                        "미용실", "이발소", "대형마트", "화장실", "공원", "커피", "음식", "레저", "호텔",  "마트", "식음료", "TV맛집", "카페", "한식", "중식", "일식", "양식"
+                        , "패밀리레스토랑", "전문음식점", "피자", "치킨", "디저트", "제과점", "패스트푸드", "교통", "버스",  "버스정류장", "지하철",
+                        "놀거리",  "영화관", "노래방", "PC방",  "공연장", "문화시설"};
 
-        tmapView.setIconVisibility(true);
 
-        tmapgps = new TMapGpsManager(MapPoi.this);
-        tmapgps.setMinTime(1000);
-        tmapgps.setMinDistance(5);
-        tmapgps.setProvider(tmapgps.NETWORK_PROVIDER);
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
-        //tmapgps.setProvider(tmapgps.GPS_PROVIDER); //gps로 현 위치 잡기
-        tmapgps.OpenGps();
+                linearLayoutTmap = (LinearLayout) findViewById(R.id.linearLayoutTmap);
+                tmapView = new TMapView(this);
 
-        TMapPoint point = new TMapPoint(37.5443003, 126.9722328); //아무위치나..
+                tmapView.setSKTMapApiKey("42856b74-3604-474f-ad72-df6d1b5aaca0");
+                linearLayoutTmap.addView(tmapView);
 
-        try {
-        TMapData tmapdata = new TMapData();
+                tmapView.setTMapPoint(avglat, avglng);
+                tmapView.setIconVisibility(true);
 
-        tmapdata.findAroundNamePOI(point, "편의점", 1, 99, new TMapData.FindAroundNamePOIListenerCallback() {
-@Override
-public void onFindAroundNamePOI(ArrayList<TMapPOIItem> poiItem) {
-        for(int i = 0; i < poiItem.size(); i++) {
-        TMapMarkerItem tItem = new TMapMarkerItem(); //마커아이템생성
-        Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.icon2); //마커비트맵생성
-        tItem.setIcon(bitmap); // 마커 아이콘 지정
-        tItem.setPosition(0.5f, 1.0f); // 마커의 중심점을 중앙, 하단으로 설정
-        TMapPOIItem item = poiItem.get(i);
-        Log.d("편의시설", item.getPOIName());
-        Log.d("lat", String.valueOf(latitude));
-        TMapPoint point = item.getPOIPoint();
-        //Log.i("point", point.toString());
-        tItem.setTMapPoint(point);
-        tmapView.addMarkerItem("tItem"+i, tItem);
+
+
+                //tmapgps = new TMapGpsManager(MapPoi.this);
+                //tmapgps.setMinTime(1000);
+                //tmapgps.setMinDistance(5);
+                //tmapgps.setProvider(tmapgps.NETWORK_PROVIDER);
+
+                //tmapgps.setProvider(tmapgps.GPS_PROVIDER); //gps로 현 위치 잡기
+                //tmapgps.OpenGps();
+
+                Spinner spinner = (Spinner) findViewById(R.id.spinner);
+                ArrayAdapter<String> adapter;
+                adapter= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+                spinner.setAdapter(adapter);
+
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                TMapPoint point = new TMapPoint(avglat, avglng); //아무위치나..
+                                String categoryname = list[position];
+
+                                try {
+                                        TMapData tmapdata = new TMapData();
+
+                                        tmapdata.findAroundNamePOI(point, categoryname, 1, 99, new TMapData.FindAroundNamePOIListenerCallback() {
+                                                @Override
+                                                public void onFindAroundNamePOI(ArrayList<TMapPOIItem> poiItem) {
+                                                        for(int i = 0; i < poiItem.size(); i++) {
+                                                                TMapMarkerItem tItem = new TMapMarkerItem(); //마커아이템생성
+                                                                Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.icon2); //마커비트맵생성
+                                                                tItem.setIcon(bitmap); // 마커 아이콘 지정
+                                                                tItem.setPosition(0.5f, 1.0f); // 마커의 중심점을 중앙, 하단으로 설정
+                                                                TMapPOIItem item = poiItem.get(i);
+                                                                Log.d("편의시설", item.getPOIName());
+                                                                Log.d("lat", String.valueOf(latitude));
+                                                                TMapPoint point = item.getPOIPoint();
+                                                                //Log.i("point", point.toString());
+                                                                tItem.setTMapPoint(point);
+                                                                tmapView.addMarkerItem("tItem"+i, tItem);
+                                                        }
+                                                }
+                                        });
+
+                                }catch (Exception e){
+                                        e.printStackTrace();
+                                }
+
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                });
+
+
+
         }
-        }
-        });
-
-        }catch (Exception e){
-        e.printStackTrace();
-        }
-
-        }
-        }
+}
 
