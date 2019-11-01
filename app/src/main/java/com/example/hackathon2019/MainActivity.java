f9dca8e10b344a1078755f74bef3887965485810
jiwon_h98@naver.com <jiwon_h98@naver.com>
2019-11-01 21:30:40 +0900

    현재위치표시

--- app/src/main/java/com/example/hackathon2019/MainActivity.java
+++ app/src/main/java/com/example/hackathon2019/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.hackathon2019;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
+import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapView;
 
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout linearLayoutTmap;
+    TMapView tmapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +26,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         linearLayoutTmap = (LinearLayout) findViewById(R.id.linearLayoutTmap);
-        TMapView tmapView = new TMapView(this);
+        tmapView = new TMapView(this);
 
         tmapView.setSKTMapApiKey("42856b74-3604-474f-ad72-df6d1b5aaca0");
         linearLayoutTmap.addView(tmapView);
+
+        tmapView.setIconVisibility(true);
+
+        setGps();
+
+    }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+
+            if (location != null) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                tmapView.setLocationPoint(longitude, latitude);
+                tmapView.setCenterPoint(longitude, latitude);
+            }
+
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+    };
+
+    public void setGps() {
+        final LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자(실내에선 NETWORK_PROVIDER 권장)
+                1000, // 통지사이의 최소 시간간격 (miliSecond)
+                1, // 통지사이의 최소 변경거리 (m)
+                mLocationListener);
     }
 }
