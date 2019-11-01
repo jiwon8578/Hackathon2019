e80fe17c7657ded9237c2f31fc2d8ecb85920487
jiwon_h98@naver.com <jiwon_h98@naver.com>
2019-11-01 23:50:12 +0900

    편의시설표시

--- app/src/main/java/com/example/hackathon2019/MainActivity.java
+++ app/src/main/java/com/example/hackathon2019/MainActivity.java
@@ -16,6 +16,7 @@ import android.util.Log;
 import android.widget.LinearLayout;
 
 import com.skt.Tmap.TMapData;
+import com.skt.Tmap.TMapGpsManager;
 import com.skt.Tmap.TMapMarkerItem;
 import com.skt.Tmap.TMapPOIItem;
 import com.skt.Tmap.TMapPoint;
@@ -23,17 +24,28 @@ import com.skt.Tmap.TMapView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TMapGpsManager.onLocationChangedCallback {
 
     LinearLayout linearLayoutTmap;
     TMapView tmapView;
     double latitude;
     double longitude;
+    private TMapGpsManager tmapgps = null;
+
+    @Override
+    public void onLocationChange(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        tmapView.setLocationPoint(longitude, latitude);
+        tmapView.setCenterPoint(longitude, latitude);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
         linearLayoutTmap = (LinearLayout) findViewById(R.id.linearLayoutTmap);
         tmapView = new TMapView(this);
@@ -43,68 +55,41 @@ public class MainActivity extends AppCompatActivity {
 
         tmapView.setIconVisibility(true);
 
-        setGps();
+        tmapgps = new TMapGpsManager(MainActivity.this);
+        tmapgps.setMinTime(1000);
+        tmapgps.setMinDistance(5);
+        tmapgps.setProvider(tmapgps.NETWORK_PROVIDER);
+
+        //tmapgps.setProvider(tmapgps.GPS_PROVIDER); //gps로 현 위치 잡기
+        tmapgps.OpenGps();
+
+        TMapPoint point = new TMapPoint(37.5443003, 126.9722328); //아무위치나..
+
         try {
-            TMapMarkerItem tItem = new TMapMarkerItem();
-            tItem.setVisible(TMapMarkerItem.VISIBLE);
             TMapData tmapdata = new TMapData();
-            TMapPoint tpoint = new TMapPoint(latitude, longitude);
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.icon2);
 
-
-            ArrayList arTMapPOIItem_3 = tmapdata.findAroundNamePOI(tpoint, "맛집");
-            for (int i = 0; i < arTMapPOIItem_3.size(); i++) {
-                TMapPOIItem item = (TMapPOIItem) arTMapPOIItem_3.get(i);
-                TMapPoint point = item.getPOIPoint();
-                tItem.setTMapPoint(point);
-                tItem.setIcon(bitmap); // 마커 아이콘 지정
-                //tItem.setPosition(0.5,1.0);
-                tmapView.addMarkerItem("tItem", tItem);
-
-            }
-        }catch (Exception e){
-
-        }
-
-    }
-
-    private final LocationListener mLocationListener = new LocationListener() {
-        public void onLocationChanged(Location location) {
-
-
-            if (location != null) {
-                try {
-                    latitude = location.getLatitude();
-                    longitude = location.getLongitude();
-                    tmapView.setLocationPoint(longitude, latitude);
-                    tmapView.setCenterPoint(longitude, latitude);
-
-                }catch (Exception e){
+            tmapdata.findAroundNamePOI(point, "편의점", 1, 99, new TMapData.FindAroundNamePOIListenerCallback() {
+                @Override
+                public void onFindAroundNamePOI(ArrayList<TMapPOIItem> poiItem) {
+                    for(int i = 0; i < poiItem.size(); i++) {
+                        TMapMarkerItem tItem = new TMapMarkerItem(); //마커아이템생성
+                        Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.icon2); //마커비트맵생성
+                        tItem.setIcon(bitmap); // 마커 아이콘 지정
+                        tItem.setPosition(0.5f, 1.0f); // 마커의 중심점을 중앙, 하단으로 설정
+                        TMapPOIItem item = poiItem.get(i);
+                        Log.d("편의시설", item.getPOIName());
+                        Log.d("lat", String.valueOf(latitude));
+                        TMapPoint point = item.getPOIPoint();
+                        //Log.i("point", point.toString());
+                        tItem.setTMapPoint(point);
+                        tmapView.addMarkerItem("tItem"+i, tItem);
+                    }
                 }
+            });
 
-            }
-
-
+        }catch (Exception e){
+            e.printStackTrace();
         }
 
-        public void onProviderDisabled(String provider) {
-        }
-
-        public void onProviderEnabled(String provider) {
-        }
-
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-        }
-    };
-
-    public void setGps() {
-        final LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }
-        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자(실내에선 NETWORK_PROVIDER 권장)
-                1000, // 통지사이의 최소 시간간격 (miliSecond)
-                1, // 통지사이의 최소 변경거리 (m)
-                mLocationListener);
     }
 }
