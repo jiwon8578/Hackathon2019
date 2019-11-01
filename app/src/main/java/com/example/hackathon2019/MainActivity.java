5a52184907201e0d7c468a1d7c6c300bd06c04fc
jiwon_h98@naver.com <jiwon_h98@naver.com>
2019-11-01 21:12:59 +0900

    Tmap API 받아오기2

--- app/src/main/java/com/example/hackathon2019/MainActivity.java
+++ app/src/main/java/com/example/hackathon2019/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.hackathon2019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.skt.Tmap.TMapView;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout linearLayoutTmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linearLayoutTmap = (LinearLayout) findViewById(R.id.linearLayoutTmap);
+        TMapView tmapView = new TMapView(this);
+
+        tmapView.setSKTMapApiKey("42856b74-3604-474f-ad72-df6d1b5aaca0");
+        linearLayoutTmap.addView(tmapView);
     }
 }
